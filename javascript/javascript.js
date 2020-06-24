d5605f0f2d30c75f0602917e5690ad23a00c1984
Nasséra <45563624+DashingNass@users.noreply.github.com>
2020-06-24 14:24:15 +0200

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,73 +2,10 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
-console.log("kijken als dit werkt!");
+console.log("ik wil dit van nu wel halen!");
 
 
-
-//Voor deze code heb ik hulp gekregen van S. Niewenhuizen.
-
-/*
-document.addEventListener('keypress', function(e) {
-    console.log("key clicked: " + e.keyCode);
-    switch (e.keyCode) {
-
-            //toetscode nummer 1.
-        case 49: background_change(); break;
-
-    }
-});
-
-
-function background_change() {
-    if (document.body.style.backgroundColor == "black") {
-        document.body.style.backgroundColor = "#05346F";
-    }
-    else {
-        document.body.style.backgroundColor = "black";
-    }
-
-}
-
-*/
-
-
-
-
-
-
-
-
-//Code van de vorige versie.
-/*
-function background_change() {
-    if (document.body.style.backgroundColor == "black") {
-        document.body.style.backgroundColor = "#05346F";
-    }
-    else {
-        document.body.style.backgroundColor == "black";
-    }
-
-}
-*/
-
-
-
-/*
-
-var heading = document.querySelector('h1');
-heading.addEventListener('click', function () {
-  heading.style.backgroundColor = 'green';
-});
-*/
-
-
-
-
-
-
-
-
+//Deze code is afkomstig van w3Schools, alleen kan ik helaas de bron niet meer vinden van deze code :s
 const p = document.querySelector('p');
 p.onmouseover = logMouseOver;
 p.onmouseout = logMouseOut;
@@ -86,11 +23,14 @@ function logMouseOut() {
 
 
 
+//Voor deze code heb ik hulp gekregen van S. Niewenhuizen.
 
 
+//Er wordt een eventelistner toegevoegd aan het toetsenbord code en consol-logt als op de toest gedrukt is.
 document.addEventListener('keypress', function(e) {
     console.log("key clicked: " + e.keyCode);
     switch (e.keyCode) {
+
 
             //toetscode nummer 1.
         case 53: background_change(); break;
@@ -100,7 +40,7 @@ document.addEventListener('keypress', function(e) {
 
 
 
-
+//Bij het klikken van de toetscode 5 veranderd de achtergrond van blauw naar rebeccapurple en weer terug.
 function background_change() {
     if (document.body.style.backgroundColor == "rebeccapurple") {
         document.body.style.backgroundColor = "#05346F";
